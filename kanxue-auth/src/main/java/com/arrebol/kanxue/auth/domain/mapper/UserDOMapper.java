7a00101673fdeb46fc20469ccef9378535507dd8
Arrebol <2362552845@qq.com>
2024-06-01 22:36:51 +0800

    feat: 整合MyBatis 代码生成器插件

--- kanxue-auth/src/main/java/com/arrebol/kanxue/auth/domain/mapper/UserDOMapper.java
+++ kanxue-auth/src/main/java/com/arrebol/kanxue/auth/domain/mapper/UserDOMapper.java
@@ -4,23 +4,16 @@ import com.arrebol.kanxue.auth.domain.dataobject.UserDO;
 
 public interface UserDOMapper {
 
-    /**
-     * 根据主键 ID 查询
-     */
-    UserDO selectByPrimaryKey(Long id);
-
-    /**
-     * 根据主键 ID 删除
-     */
     int deleteByPrimaryKey(Long id);
 
-    /**
-     * 插入记录
-     */
     int insert(UserDO record);
 
-    /**
-     * 更新记录
-     */
+    int insertSelective(UserDO record);
+
+    UserDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserDO record);
+
     int updateByPrimaryKey(UserDO record);
+
 }
