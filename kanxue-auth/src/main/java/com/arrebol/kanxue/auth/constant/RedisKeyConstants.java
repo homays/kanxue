01710f3d89e396aff0055d0ac2447e458851b7fa
Arrebol <2362552845@qq.com>
2024-06-10 23:14:00 +0800

    feat: 用户验证码登录功能实现

--- kanxue-auth/src/main/java/com/arrebol/kanxue/auth/constant/RedisKeyConstants.java
+++ kanxue-auth/src/main/java/com/arrebol/kanxue/auth/constant/RedisKeyConstants.java
@@ -8,10 +8,27 @@ public class RedisKeyConstants {
     private static final String VERIFICATION_CODE_KEY_PREFIX = "verification_code:";
 
     /**
+     * 看雪全局 ID 生成器 KEY
+     */
+    public static final String KANXUE_ID_GENERATOR_KEY = "kanxue_id_generator";
+
+    /**
+     * 用户角色数据 KEY 前缀
+     */
+    private static final String USER_ROLES_KEY_PREFIX = "user:roles:";
+
+    /**
      * 构建验证码 KEY
      */
     public static String buildVerificationCodeKey(String phone) {
         return VERIFICATION_CODE_KEY_PREFIX + phone;
+    }
+
+    /**
+     * 构建用户权限 KEY
+     */
+    public static String buildUserRoleKey(String phone) {
+        return USER_ROLES_KEY_PREFIX + phone;
     }
 
 }
---
+++ kanxue-framework/kanxue-common/src/main/java/com/arrebol/framework/common/constant/CommonConstants.java
@@ -0,0 +1,7 @@
+package com.arrebol.framework.common.constant;
+
+public class CommonConstants {
+
+    public static final String NICKNAME_PREFIX = "看雪_";
+
+}
