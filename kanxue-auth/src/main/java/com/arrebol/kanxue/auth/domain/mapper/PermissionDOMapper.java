1d491ea0c3df68ccd414e958ecbf20ae65c243f0
Arrebol <2362552845@qq.com>
2024-06-10 22:33:21 +0800

    feat: 数据实体生成

---
+++ kanxue-auth/src/main/java/com/arrebol/kanxue/auth/domain/mapper/PermissionDOMapper.java
@@ -0,0 +1,17 @@
+package com.arrebol.kanxue.auth.domain.mapper;
+
+import com.arrebol.kanxue.auth.domain.dataobject.PermissionDO;
+
+public interface PermissionDOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(PermissionDO record);
+
+    int insertSelective(PermissionDO record);
+
+    PermissionDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(PermissionDO record);
+
+    int updateByPrimaryKey(PermissionDO record);
+}
---
+++ kanxue-auth/src/main/java/com/arrebol/kanxue/auth/domain/mapper/RolePermissionDOMapper.java
@@ -0,0 +1,17 @@
+package com.arrebol.kanxue.auth.domain.mapper;
+
+import com.arrebol.kanxue.auth.domain.dataobject.RolePermissionDO;
+
+public interface RolePermissionDOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(RolePermissionDO record);
+
+    int insertSelective(RolePermissionDO record);
+
+    RolePermissionDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(RolePermissionDO record);
+
+    int updateByPrimaryKey(RolePermissionDO record);
+}
