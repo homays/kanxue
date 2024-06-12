f17668e67859657792cd6bd32ffbff570491fe83
Arrebol <2362552845@qq.com>
2024-06-12 16:26:35 +0800

    feat: 用户注册的声明式事务改为编程式事务

--- kanxue-auth/src/main/java/com/arrebol/kanxue/auth/service/impl/UserServiceImpl.java
+++ kanxue-auth/src/main/java/com/arrebol/kanxue/auth/service/impl/UserServiceImpl.java
@@ -25,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -46,6 +47,8 @@ public class UserServiceImpl implements UserService {
     private UserRoleDOMapper userRoleDOMapper;
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
+    @Resource
+    private TransactionTemplate transactionTemplate;
 
     @Override
     public Response<String> loginAndRegister(UserLoginReqVO userLoginReqVO) {
@@ -62,6 +65,7 @@ public class UserServiceImpl implements UserService {
             case VERIFICATION_CODE:
                 // 验证码
                 String code = userLoginReqVO.getCode();
+
 
                 // 校验验证码是否为空
                 if (StrUtil.isBlank(code)) {
@@ -112,43 +116,50 @@ public class UserServiceImpl implements UserService {
         return Response.success(tokenInfo.tokenValue);
     }
 
-    @Transactional(rollbackFor = Exception.class)
     public Long registerUser(String phone) {
-        // 获取全局自增的看雪 ID
-        Long kanxueId = redisTemplate.opsForValue().increment(RedisKeyConstants.KANXUE_ID_GENERATOR_KEY);
+        return transactionTemplate.execute(status -> {
+            try {
+                // 获取全局自增的看雪 ID
+                Long kanxueId = redisTemplate.opsForValue().increment(RedisKeyConstants.KANXUE_ID_GENERATOR_KEY);
 
-        UserDO userDO = UserDO.builder()
-                .phone(phone)
-                .kanxueId(String.valueOf(kanxueId)) // 自动生成看雪号 ID
-                .nickname(CommonConstants.NICKNAME_PREFIX + kanxueId) // 自动生成昵称, 如：看雪_10000
-                .status(StatusEnum.ENABLE.getValue()) // 状态为启用
-                .createTime(LocalDateTime.now())
-                .updateTime(LocalDateTime.now())
-                .isDeleted(DeletedEnum.NO.getValue()) // 逻辑删除
-                .build();
+                UserDO userDO = UserDO.builder()
+                        .phone(phone)
+                        .kanxueId(String.valueOf(kanxueId)) // 自动生成看雪号 ID
+                        .nickname(CommonConstants.NICKNAME_PREFIX + kanxueId) // 自动生成昵称, 如：看雪_10000
+                        .status(StatusEnum.ENABLE.getValue()) // 状态为启用
+                        .createTime(LocalDateTime.now())
+                        .updateTime(LocalDateTime.now())
+                        .isDeleted(DeletedEnum.NO.getValue()) // 逻辑删除
+                        .build();
 
-        // 保存用户信息
-        userDOMapper.insert(userDO);
+                // 保存用户信息
+                userDOMapper.insert(userDO);
 
-        // 获取刚刚添加入库的用户 ID
-        Long userId = userDO.getId();
+                // 获取刚刚添加入库的用户 ID
+                Long userId = userDO.getId();
 
-        // 给该用户分配一个默认角色
-        UserRoleDO userRoleDO = UserRoleDO.builder()
-                .userId(userId)
-                .roleId(RoleConstants.COMMON_USER_ROLE_ID)
-                .createTime(LocalDateTime.now())
-                .updateTime(LocalDateTime.now())
-                .isDeleted(DeletedEnum.NO.getValue())
-                .build();
-        userRoleDOMapper.insert(userRoleDO);
+                // 给该用户分配一个默认角色
+                UserRoleDO userRoleDO = UserRoleDO.builder()
+                        .userId(userId)
+                        .roleId(RoleConstants.COMMON_USER_ROLE_ID)
+                        .createTime(LocalDateTime.now())
+                        .updateTime(LocalDateTime.now())
+                        .isDeleted(DeletedEnum.NO.getValue())
+                        .build();
+                userRoleDOMapper.insert(userRoleDO);
 
-        // 将该用户的角色 ID 存入 Redis 中
-        List<Long> roles = new ArrayList<>();
-        roles.add(RoleConstants.COMMON_USER_ROLE_ID);
-        String userRolesKey = RedisKeyConstants.buildUserRoleKey(phone);
-        redisTemplate.opsForValue().set(userRolesKey, JsonUtil.toJsonString(roles));
+                // 将该用户的角色 ID 存入 Redis 中
+                List<Long> roles = new ArrayList<>();
+                roles.add(RoleConstants.COMMON_USER_ROLE_ID);
+                String userRolesKey = RedisKeyConstants.buildUserRoleKey(phone);
+                redisTemplate.opsForValue().set(userRolesKey, JsonUtil.toJsonString(roles));
 
-        return userId;
+                return userId;
+            } catch (Exception e) {
+                status.setRollbackOnly(); // 标记事务为回滚
+                log.error("==> 系统注册用户异常: ", e);
+                return null;
+            }
+        });
     }
 }
