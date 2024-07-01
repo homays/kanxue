36c6ca86299e12a8fca1e31fca9a64199960d2c2
Arrebol <2362552845@qq.com>
2024-07-01 19:37:48 +0800

    feat: 网关搭建

---
+++ kanxue-gateway/src/main/java/com/arrebol/kanxue/gateway/KanxueGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.arrebol.kanxue.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanxueGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KanxueGatewayApplication.class, args);
+    }
+}
