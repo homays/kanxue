92597c6aeddf9502ae0bfc921cc3413654d57d1e
Arrebol <2362552845@qq.com>
2024-07-02 11:06:09 +0800

    feat: 网关全局异常处理

--- kanxue-gateway/src/main/java/com/arrebol/kanxue/gateway/auth/SaTokenConfigure.java
+++ kanxue-gateway/src/main/java/com/arrebol/kanxue/gateway/auth/SaTokenConfigure.java
@@ -28,7 +28,7 @@ public class SaTokenConfigure {
                     ;
 
                     // 权限认证 -- 不同模块, 校验不同权限
-                    SaRouter.match("/auth/user/logout", r -> StpUtil.checkPermission("app:note:publish"));
+                    SaRouter.match("/auth/user/logout", r -> StpUtil.checkPermission("app:note:delete"));
                     // SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
                     // SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
                     // SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
@@ -36,9 +36,9 @@ public class SaTokenConfigure {
                     // 更多匹配 ...  */
                 })
                 // 异常处理方法：每次setAuth函数出现异常时进入
-                .setError(e -> {
+                /*.setError(e -> {
                     return SaResult.error(e.getMessage());
-                })
+                })*/
                 ;
     }
 }
