5a807491404da9eccf0af9018520b49542204520
Arrebol <2362552845@qq.com>
2024-07-03 21:09:38 +0800

    feat: 搭建oss

---
+++ kanxue-oss/kanxue-oss-biz/src/main/java/com/arrebol/kanxue/oss/biz/KanxueOssBizApplication.java
@@ -0,0 +1,11 @@
+package com.arrebol.kanxue.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanxueOssBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KanxueOssBizApplication.class, args);
+    }
+}
