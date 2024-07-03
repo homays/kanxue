febc031bc9506e89b7718e81d3ca2eb53e9cd583
Arrebol <2362552845@qq.com>
2024-07-03 11:18:25 +0800

    feat: 自定义 starter 封装用户上下文

--- kanxue-framework/kanxue-spring-boot-starter-biz-context/src/main/java/com/arrebol/framework/biz/context/holder/LoginUserContextHolder.java
+++ kanxue-framework/kanxue-spring-boot-starter-biz-context/src/main/java/com/arrebol/framework/biz/context/holder/LoginUserContextHolder.java
@@ -1,4 +1,4 @@
-package com.arrebol.kanxue.auth.filter;
+package com.arrebol.framework.biz.context.holder;
 
 import com.alibaba.ttl.TransmittableThreadLocal;
 import com.arrebol.framework.common.constant.GlobalConstants;
@@ -7,17 +7,11 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Objects;
 
-/**
- * 登录用户上下文
- * 已弃用 在framework 模块中封装了 starter
- */
-@Deprecated
 public class LoginUserContextHolder {
 
     // 初始化一个 ThreadLocal 变量
     private static final ThreadLocal<Map<String, Object>> LOGIN_USER_CONTEXT_THREAD_LOCAL
             = TransmittableThreadLocal.withInitial(HashMap::new);
-
 
     /**
      * 设置用户 ID
@@ -38,7 +32,7 @@ public class LoginUserContextHolder {
     }
 
     /**
-     * 删除 ThreadLocal 
+     * 删除 ThreadLocal
      */
     public static void remove() {
         LOGIN_USER_CONTEXT_THREAD_LOCAL.remove();
