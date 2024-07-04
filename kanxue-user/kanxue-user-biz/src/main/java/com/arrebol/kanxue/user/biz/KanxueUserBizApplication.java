eb6f9071078ea0baa8834caba5e618383b20a948
Arrebol <2362552845@qq.com>
2024-07-04 10:37:01 +0800

    feat: 用户服务搭建，创建用户实体

--- kanxue-user/kanxue-user-biz/src/main/java/com/arrebol/kanxue/user/biz/KanxueUserBizApplication.java
+++ kanxue-user/kanxue-user-biz/src/main/java/com/arrebol/kanxue/user/biz/KanxueUserBizApplication.java
@@ -1,9 +1,11 @@
 package com.arrebol.kanxue.user.biz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.arrebol.kanxue.user.biz.domain.mapper")
 public class KanxueUserBizApplication {
     public static void main(String[] args) {
         SpringApplication.run(KanxueUserBizApplication.class, args);
