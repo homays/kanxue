282a41f79793edabffce0296695e2263c2114aa5
Arrebol <2362552845@qq.com>
2024-07-11 11:21:33 +0800

    style: 删除冗余代码

--- kanxue-auth/src/main/java/com/arrebol/kanxue/auth/KanxueAuthApplication.java
+++ kanxue-auth/src/main/java/com/arrebol/kanxue/auth/KanxueAuthApplication.java
@@ -1,12 +1,10 @@
 package com.arrebol.kanxue.auth;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@MapperScan("com.arrebol.kanxue.auth.domain.mapper")
 @EnableFeignClients(basePackages = "com.arrebol.kanxue")
 public class KanxueAuthApplication {
 
--- kanxue-auth/src/main/java/com/arrebol/kanxue/auth/alarm/AlarmInterface.java
+++
@@ -1,9 +0,0 @@
-package com.arrebol.kanxue.auth.alarm;
-
-public interface AlarmInterface {
-
-    /**
-     * 发送告警信息
-     */
-    boolean send(String message);
-}
