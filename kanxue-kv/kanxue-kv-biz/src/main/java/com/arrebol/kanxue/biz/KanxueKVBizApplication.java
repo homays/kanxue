735f6f07ef87fda9ab29f30485ee756f0d4b91c6
Arrebol <2362552845@qq.com>
2024-08-14 16:52:29 +0800

    feat: KV键值存储微服务搭建

---
+++ kanxue-kv/kanxue-kv-biz/src/main/java/com/arrebol/kanxue/biz/KanxueKVBizApplication.java
@@ -0,0 +1,17 @@
+package com.arrebol.kanxue.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * KV启动类
+ *
+ * @author Arrebol
+ * @date 2024/8/14
+ */
+@SpringBootApplication
+public class KanxueKVBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KanxueKVBizApplication.class, args);
+    }
+}
